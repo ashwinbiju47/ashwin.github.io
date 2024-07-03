e814edef7ac99e93eef4e14f1a0cd5d46f0a1a73
Ashwin <ashwin.nltech@gmail.com>
2024-07-04 00:13:17 +0530

    implemented qualifications component

--- src/components/qualification/Qualification.tsx
+++ src/components/qualification/Qualification.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './qualification.css'
 
 const Qualification = () => {
+
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index: any): void => {
+    setToggleState(index);
+  };
+
+
   return (
     <section className="qualification section" id="qualification">
       <h2 className="section__title">Qualification</h2>
@@ -10,102 +18,106 @@ const Qualification = () => {
       <div className="qualification__container container">
         <div className="qualification__tabs">
 
-          <div className="qualification__buttons qualification__active button--flex">
-            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+          <div
+            className={toggleState === 1
+              ? "qualification__button qualification__active button--flex"
+              : "qualification__button button--flex"
+            }
+
+            onClick={() => { toggleTab(1) }}
+
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>
             Education
           </div>
 
-          <div className="qualification__buttons qualification__active button--flex">
-            <i className="uil uil-briefcase-alt qualification__icon"></i>{" "}
+          <div
+            className={toggleState === 2
+              ? "qualification__button qualification__active button--flex"
+              : "qualification__button button--flex"
+            }
+            onClick={() => { toggleTab(2) }}
+          >
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
             Experience
           </div>
 
-          <div className="qualification__sections">
+        </div>
 
-            <div className="qualification__content">
+        <div className="qualification__sections">
 
-              {/* Data 1 */}
-              <div className="qualification__data">
+          <div 
+          className={toggleState === 1
+            ? "qualification__content qualification__content-active "
+            : "qualification__content "
+          }
+          onClick={()=>{toggleTab(1)}}
+          >
 
-                <div>
-                  <h3 className="qualification__title">B.Tech: Computer Science and Engineering</h3>
-                  <span className="qualification__subtitle">Mar Baselios Institute of Technology and Science, Kothamangalam, Ernakulam, Kerala</span>
-                  <div className="qualification__calendar">
-                    <i className="uil uil-calendar-alt"></i> 2019-2023
-                  </div>
+            {/* Data 1 */}
+            <div className="qualification__data">
+
+              <div>
+                <h3 className="qualification__title">B.Tech: Computer Science and Engineering</h3>
+                <span className="qualification__subtitle">Mar Baselios Institute of Technology and Science, Kothamangalam, Ernakulam, Kerala</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2019-2023
                 </div>
-
-
-                <div>
-                  <span className="qualification__rounder"></span>
-                  <span className="qualification__line"></span>
-                </div>
-
               </div>
 
-              {/* Data 2 */}
-              {/* <div className="qualification__data">
 
-                <div>
-
-                </div>
-                
-                <div>
-                  <span className="qualification__rounder"></span>
-                  <span className="qualification__line"></span>
-                </div>
-
-                <div>
-                  <h3 className="qualification__title"></h3>
-                  <span className="qualification__subtitle"></span>
-                  <div className="qualification__calendar">
-                    <i className="uil uil-calendar-alt"></i> 2019-2023
-                  </div>
-                </div>
-
-              </div> */}
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
 
             </div>
 
-            {/* Experience */}
-            <div className="qualification__content qualification__content-active ">
+          </div>
 
-              {/* Data 1 */}
-              <div className="qualification__data">
+          {/* Experience */}
+          <div 
+          className={toggleState === 2
+            ? "qualification__content qualification__content-active "
+            : "qualification__content "
+          }
+          onClick={()=>{toggleTab(2)}}
+          >
 
-                <div>
-                  <h3 className="qualification__title">Jr. Software Developer</h3>
-                  <span className="qualification__subtitle">Probablity Gaming, Goa, India</span>
-                  <div className="qualification__calendar">
-                    <i className="uil uil-calendar-alt"></i> 08/2023 - Present
-                  </div>
+            {/* Data 1 */}
+            <div className="qualification__data">
+
+              <div>
+                <h3 className="qualification__title">Jr. Software Developer</h3>
+                <span className="qualification__subtitle">Probablity Gaming, Goa, India</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 08/2023 - Present
                 </div>
-
-
-                <div>
-                  <span className="qualification__rounder"></span>
-                  <span className="qualification__line"></span>
-                </div>
-
               </div>
 
-              {/* Data 2 */}
-              <div className="qualification__data">
 
-                <div>
-                  <h3 className="qualification__title">Full Stack Developer (MERN) </h3>
-                  <span className="qualification__subtitle">Freelance</span>
-                  <div className="qualification__calendar">
-                    <i className="uil uil-calendar-alt"></i> 2023
-                  </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+            </div>
+
+            {/* Data 2 */}
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Full Stack Developer (MERN) </h3>
+                <span className="qualification__subtitle">Freelance</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2023
                 </div>
-
-
-                <div>
-                  <span className="qualification__rounder"></span>
-                  <span className="qualification__line"></span>
-                </div>
-
               </div>
 
             </div>

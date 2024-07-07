f370b103120a964bf3e49b50d6f666805007fdde
Ashwin <ashwin.nltech@gmail.com>
2024-07-07 22:02:26 +0530

    created Contact link in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -46,14 +46,14 @@ const Header = () => {
                 <i className="uil uil-scenery nav__icon"></i>
                 Portfolio
               </a>
-            </li>
+            </li>*/}
 
             <li className="nav__item">
               <a href="#contact" className="nav__link">
                 <i className="uil uil-message nav__icon"></i>
                 Contact
               </a>
-            </li> */}
+            </li> 
           </ul>
 
           <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>

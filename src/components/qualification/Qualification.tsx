5ecc083157d15c3384bd5232f00599167da39218
4shx1 <ashwin.biju.k@gmail.com>
2025-01-23 01:12:43 +0530

    added squadra media to my qualifications and experience page

--- src/components/qualification/Qualification.tsx
+++ src/components/qualification/Qualification.tsx
@@ -17,92 +17,79 @@ const Qualification = () => {
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
-
           <div
-            className={toggleState === 1
-              ? "qualification__button qualification__active button--flex"
-              : "qualification__button button--flex"
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
             }
-
-            onClick={() => { toggleTab(1) }}
-
+            onClick={() => {
+              toggleTab(1);
+            }}
           >
             <i className="uil uil-graduation-cap qualification__icon"></i>
             Education
           </div>
 
           <div
-            className={toggleState === 2
-              ? "qualification__button qualification__active button--flex"
-              : "qualification__button button--flex"
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
             }
-            onClick={() => { toggleTab(2) }}
+            onClick={() => {
+              toggleTab(2);
+            }}
           >
             <i className="uil uil-briefcase-alt qualification__icon"></i>
             Experience
           </div>
-
         </div>
 
         <div className="qualification__sections">
-
-          <div 
-          className={toggleState === 1
-            ? "qualification__content qualification__content-active "
-            : "qualification__content "
-          }
-          onClick={()=>{toggleTab(1)}}
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active "
+                : "qualification__content "
+            }
+            onClick={() => {
+              toggleTab(1);
+            }}
           >
-
             {/* Data 1 */}
             <div className="qualification__data">
-
               <div>
-                <h3 className="qualification__title">B.Tech: Computer Science and Engineering</h3>
-                <span className="qualification__subtitle">Mar Baselios Institute of Technology and Science, Kothamangalam, Ernakulam, Kerala</span>
+                <h3 className="qualification__title">
+                  B.Tech: Computer Science and Engineering
+                </h3>
+                <span className="qualification__subtitle">
+                  Mar Baselios Institute of Technology and Science,
+                  Kothamangalam, Ernakulam, Kerala
+                </span>
                 <div className="qualification__calendar">
                   <i className="uil uil-calendar-alt"></i> 2019-2023
                 </div>
               </div>
 
-
               <div>
                 <span className="qualification__rounder"></span>
                 <span className="qualification__line"></span>
               </div>
-
             </div>
-
           </div>
 
           {/* Experience */}
-          <div 
-          className={toggleState === 2
-            ? "qualification__content qualification__content-active "
-            : "qualification__content "
-          }
-          onClick={()=>{toggleTab(2)}}
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active "
+                : "qualification__content "
+            }
+            onClick={() => {
+              toggleTab(2);
+            }}
           >
-
-            {/* Data 1 */}
-            <div className="qualification__data">
-
-              <div>
-                <h3 className="qualification__title">Jr. Software Developer</h3>
-                <span className="qualification__subtitle">Probablity Gaming, Goa, India</span>
-                <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 08/2023 - Present
-                </div>
-              </div>
-
-
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-
-            </div>
-
             {/* Data 2 */}
             <div className="qualification__data">
               <div></div>
@@ -113,22 +100,60 @@ const Qualification = () => {
               </div>
 
               <div>
-                <h3 className="qualification__title">Full Stack Developer (MERN) </h3>
-                <span className="qualification__subtitle">Freelance</span>
+                <h3 className="qualification__title">
+                  Backend Developer{" "}
+                </h3>
+                <span className="qualification__subtitle">
+                  Squadra Media, Bengaluru, Karnataka, India 
+                </span>
                 <div className="qualification__calendar">
-                  <i className="uil uil-calendar-alt"></i> 2023
+                  <i className="uil uil-calendar-alt"></i> 09/2024 - Present
+                </div>
+              </div>
+            </div>
+
+            {/* Data 1 */}
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Jr. Software Developer</h3>
+                <span className="qualification__subtitle">
+                  Probablity Gaming, Goa, India
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 08/2023 - 09/2024
                 </div>
               </div>
 
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
             </div>
 
-          </div>
+            {/* Data 0 */}
+            <div className="qualification__data">
+              <div></div>
 
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">
+                  Full Stack Developer (MERN){" "}
+                </h3>
+                <span className="qualification__subtitle">Freelance</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 02/2023 - 07/2023
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
-
-  )
+  );
 }
 
 export default Qualification
